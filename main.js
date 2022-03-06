35318eaa98f0e7782183f61bc642df582cc824d6
Amshul-korlepara <82725729+Amshul-korlepara@users.noreply.github.com>
2022-03-05 19:12:30 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,41 @@ var random_number = Math.floor((Math.random()*quick_draw_data_set.length)+1);
 console.log(quick_draw_data_set[random_number]);
 document.getElementById("Sketchtobedrawn").innerHTML= "Sketch to be drawn: "+sketch;
 
-timer_counter=0;
-timer_check=""; 
-drawn_sketch="";
-answer_holder="";
-score=0;
+var timer_counter=0;
+var timer_check=""; 
+var drawn_sketch="";
+var answer_holder="";
+var score=0;
+function setup(){
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+function  draw(){
+check_sketch();
+if(drawn_sketch==sketch){
+answer_holder="set";
+score++;
+document.getElementById("score").innerHTML=score;
+
+}
+
+ }
+ function check_sketch(){
+     timer_counter++;
+    document.getElementById("timer").innerHTML="Timer: "+timer_counter;
+    console.log(timer_counter);
+    if(timer_counter>400){
+        timer_counter=0;
+
+        timer_check="completed";
+    }
+    if(timer_check=="completed" || answer_holder=="set"){
+         timer_check="";
+        answer_holder="";
+        updateCanvas()
+    }
+ }
+ function updateCanvas(){
+
+ }
